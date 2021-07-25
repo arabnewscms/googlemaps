793ad86a6c588275fd8b9be2eb03bc210fdcaf4d
php anonymous <php.anonymous1@gmail.com>
2021-07-25 14:25:58 +0200

    Add files via upload

---
+++ lesson 2/js/map.js
@@ -0,0 +1,20 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat:30.033333, lng: 31.233334 },
+    zoom: 8,
+  });
+
+  setTimeout(function(){
+      if($('#map').find('.dismissButton').length == 1){
+       $('#map').children('div:nth-of-type(2)').remove();
+      }
+    },1000);
+}
+
+$(document).ready(function(){
+
+initMap();
+
+});
